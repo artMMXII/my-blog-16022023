52b7f6404832e4cb082a3167f1657371528b2bde
Arthur Khasiev <hasiev_artur@mail.ru>
2023-02-17 19:57:37 +0700

    create new model, add note page, some problem with color of note text

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from blog.views import index
+from .views import PostView, AddNote
 
 urlpatterns = [
-    path('', index, name='home')
+    path('', index, name='home'),
+    path('note/', PostView.as_view(), name='note'),
+    path('add_note/', AddNote.as_view(), name='add_note')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
+from .models import Note
+from .forms import AddNoteForm
 
 
 def index(request):
     context = {'title': 'Главная страница'}
     return render(request, 'blog/index.html', context=context)
+
+
+class PostView(ListView):
+    model = Note
+    template_name = 'blog/note.html'
+
+
+class AddNote(CreateView):
+    form_class = AddNoteForm
+    template_name = 'blog/add_note.html'
+    success_url = reverse_lazy('home')
+
+
+

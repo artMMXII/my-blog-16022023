e9eb9b8cea17bba2b0b075c58d3f64059d930106
Arthur Khasiev <hasiev_artur@mail.ru>
2023-02-18 14:27:51 +0700

    realise color of text and title in note

--- blog/forms.py
+++ blog/forms.py
@@ -5,3 +5,7 @@ class AddNoteForm(forms.ModelForm):
     class Meta:
         model = Note
         fields = ('title', 'content')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols': 60, 'rows': 5})
+        }
--- blog/models.py
+++ blog/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class Note(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField(blank=True)
+    title = models.CharField(max_length=50, verbose_name='Заголовок')
+    content = models.TextField(blank=True, verbose_name='Текст')
